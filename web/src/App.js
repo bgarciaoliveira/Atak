cf5aef1b30faa60d6034aeceb50c2f7bced29c34
bgarciaoliveira <brunogarcia212@gmail.com>
2019-04-29 04:03:35 -0300

    Criacao e estilizacao do Header

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header'
+import api from './services/api'
+
+import './styles.css'
+
+const App = () => (
+  <div className="App">
+    <Header /> 
+  </div>
+
+)
+
+export default App;
