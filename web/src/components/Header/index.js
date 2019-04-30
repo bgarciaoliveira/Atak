e55aa513ad14c031616d88634cccedad0dae6526
bgarciaoliveira <brunogarcia212@gmail.com>
2019-04-29 23:01:12 -0300

    Icone, logo e header title

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 import './styles.css'
 
 const Header = () => (
-    <header id="main-header">BSearch</header>
+    <header id="main-header">
+    <img alt="logo" src={ require('./logo.png') } />
+    Search</header>
 )
 
 export default Header
